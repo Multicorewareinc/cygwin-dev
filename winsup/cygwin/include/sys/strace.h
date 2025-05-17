6d47c24bc86b6617b13a2c19ce5da4610f8f33a2
Radek Bartoň <radek.barton@microsoft.com>
2025-05-17 12:52:31 +0200

    Revert "Activate `strace`"
    This reverts commit 724de39c352eac5dca8b334cdca51fe41e324573.

--- winsup/cygwin/include/sys/strace.h
+++ winsup/cygwin/include/sys/strace.h
@@ -38,7 +38,6 @@ class strace
   unsigned char _active;
 public:
   void activate (bool);
-  void set_active (unsigned active) {_active = active;}
   strace () {}
   int microseconds ();
   int version;
@@ -92,7 +91,6 @@ extern "C" {
 
 void small_printf (const char *, ...);
 void strace_printf (unsigned, const char *func, const char *, ...);
-void strace_set_active (unsigned);
 
 #ifdef __cplusplus
 }
--- winsup/cygwin/strace.cc
+++ winsup/cygwin/strace.cc
@@ -312,11 +312,6 @@ strace_printf (unsigned category, const char *func, const char *fmt, ...)
     }
 }
 
-extern "C" void
-strace_set_active (unsigned active)
-{
-  strace.set_active (active);
-}
 static NO_COPY struct tab
 {
   int v;
